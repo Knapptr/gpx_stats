953f4c39ee2790aaa48cd197f180bd245879c036
knapptr <knapptr@gmail.com>
2023-04-10 07:35:43 -0400

    Make Module out of parser

--- src/ele.rs
+++ src/ele.rs
@@ -1,20 +1,89 @@
-pub fn get_elevation_change<T: Iterator<Item = f32>>(eles: T) -> (f32, f32) {
+pub fn get_elevation_change(eles: &Vec<f32>) -> (f32, f32) {
     let mut gain = 0f32;
     let mut loss = 0f32;
     let mut prev: Option<f32> = None;
     for ele in eles {
         if let Some(previous_elevation) = prev {
             let diff = (previous_elevation - ele).abs();
-            if diff > 10f32 {
-                if previous_elevation < ele {
-                    gain += ele;
-                }
-                if previous_elevation > ele {
-                    loss += ele;
-                }
+            if previous_elevation < *ele {
+                gain += diff;
+            }
+            if previous_elevation > *ele {
+                loss += diff;
             }
         }
-        prev = Some(ele)
+        prev = Some(*ele)
     }
     (gain, loss)
+}
+
+pub fn smooth_ele_data(eles: Vec<f32>, mut sample_size: usize) -> Vec<f32> {
+    // establish maximum index
+    let max_index = eles.len() - 1;
+    // handle odd sample_size
+    sample_size = if sample_size % 2 == 0 {
+        sample_size + 1
+    } else {
+        sample_size
+    };
+    // number of samples on either side of center point
+    let each_side = (sample_size - 1) / 2;
+    // init bound indexes
+    let mut lower_bound = 0usize;
+    let mut higher_bound = if lower_bound + each_side > (max_index) {
+        max_index
+    } else {
+        lower_bound + each_side
+    };
+    // init running sum to avg of first point in sample bounds
+    let mut sum: f32 = eles.iter().take(each_side + 1).sum::<f32>()
+        + (eles.iter().take(1).sum::<f32>() * each_side as f32);
+
+    // init return vec
+    let mut smoothed_data = vec![sum / sample_size as f32];
+    // sum -= eles.iter().take(1).sum::<f32>();
+
+    // println!("Each Side: {} | Sample Size: {}\n", each_side, sample_size);
+    ////
+    ////
+    for (current_index, _element) in eles.iter().enumerate().skip(1) {
+        // println!(
+        // "\nAt: {} ({})\nSubtracting {} from sum of {}",
+        // _element, current_index, eles[lower_bound], sum
+        // );
+        sum -= eles[lower_bound];
+        // move lower_bound up 1 if significant
+        lower_bound = if current_index as isize - each_side as isize >= 0 {
+            current_index - each_side
+        } else {
+            0
+        };
+        // move higher bound if possible
+        higher_bound = max_index.min(higher_bound + 1);
+        sum += eles[higher_bound];
+        // println!(
+        //     "Sum: {} | lower bound: {} | upper bound: {}",
+        //     sum, lower_bound, higher_bound
+        // );
+        smoothed_data.push(sum / sample_size as f32);
+    }
+    ////
+    ////
+    smoothed_data
+}
+
+#[cfg(test)]
+#[test]
+fn smoothed_data_transform() {
+    let test_data: Vec<f32> = vec![2.0, 4.0, 6.0, 8.0, 10.0, 12.0];
+    let smoothed_data = smooth_ele_data(test_data, 2);
+    let expected_data = vec![2.67, 4.0, 6.0, 8.0, 10.0, 11.33];
+
+    // Comparing rounded strings here, because I can't figure out a better way to compare
+    // floating points rn.
+    for (i, el) in smoothed_data.iter().enumerate() {
+        let rounded_smoothed = format!("{:.2}", el);
+        // println!("comparing {} to {}", expected_data[i], rounded_smoothed);
+        assert_eq!(rounded_smoothed, format!("{:.2}", expected_data[i]));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,101 @@
-use std::{env, fs::read_to_string, path::Path};
+use plotters::prelude::*;
+use std::{env, fs::read_to_string, ops::Range, path::Path};
 mod ele;
+mod parse;
 
 use xmlparser::Token;
 fn main() {
-    let doc = read_to_string("daloop.gpx").unwrap();
-    let tokens = xmlparser::Tokenizer::from(doc.as_str());
-    let mut eles: Vec<f32> = vec![];
-    let mut prev: Option<String> = None;
-    for token in tokens {
-        match token.unwrap() {
-            Token::Text { text } => {
-                if prev == Some("ele".to_string()) {
-                    eles.push(text.parse::<f32>().unwrap());
-                }
-            }
-            Token::ElementStart {
-                prefix,
-                local,
-                span,
-            } => {
-                prev = Some(local.to_string());
-            }
-            _ => (),
-        }
-    }
-    let (gain, loss) = ele::get_elevation_change(eles.into_iter());
-
-    println!("Gain: {}\nLoss: {}\nDiff:{}", gain, loss, gain - loss);
+    parse::parse_it();
 }
+// fn main() {
+//     // probably move to CLI
+//     let file_name = "daloop.gpx";
+//     let smoothing_amount = 100;
+//     // let doc = read_to_string("daloop.gpx").unwrap();
+//     // let doc = read_to_string("Formatted_sd.gpx").unwrap();
+//     let doc = read_to_string(file_name).unwrap();
+//     let tokens = xmlparser::Tokenizer::from(doc.as_str());
+//     let mut eles: Vec<f32> = vec![];
+//     let mut min: f32 = f32::MAX;
+//     let mut max: f32 = 0.0;
+//     let mut prev: Option<String> = None;
+//     for token in tokens {
+//         match token.unwrap() {
+//             Token::Text { text } => {
+//                 if prev == Some("ele".to_string()) {
+//                     let content = text.trim();
+//                     if content.len() > 0 {
+//                         let parsed_ele = content.parse::<f32>().unwrap();
+//                         if parsed_ele > max {
+//                             max = parsed_ele;
+//                         };
+//                         if parsed_ele < min {
+//                             min = parsed_ele;
+//                         }
+//                         eles.push(parsed_ele);
+//                     }
+//                 }
+//             }
+//             Token::ElementStart {
+//                 prefix,
+//                 local,
+//                 span,
+//             } => {
+//                 prev = Some(local.to_string());
+//             }
+//             _ => (),
+//         }
+//     }
+//     let original_data = eles.clone();
+//     // for ele in original_data.iter() {
+//     //     println!("ELE: {}", ele);
+//     // }
+//     let smoothed_data = ele::smooth_ele_data(eles, smoothing_amount);
+//     let sample_count = smoothed_data.len();
+//     let (s_gain, s_loss) = ele::get_elevation_change(&smoothed_data);
+//     let (r_gain, r_loss) = ele::get_elevation_change(&original_data);
+//     println!("Samples: {sample_count}");
+
+//     println!(
+//         "Raw: Gain: {}\nLoss: {}\nDiff:{}\nmax: {}, min:{}",
+//         r_gain,
+//         r_loss,
+//         r_gain - r_loss,
+//         max,
+//         min
+//     );
+//     println!(
+//         "Smoothed: Gain: {}\nLoss: {}\nDiff:{}",
+//         s_gain,
+//         s_loss,
+//         s_gain - s_loss
+//     );
+
+//     // Plotting
+//     let upper_padding = 10.0;
+//     let lower_padding = 10.0;
+//     let root_area = plotters::prelude::SVGBackend::new("1.5.svg", (1028, 720)).into_drawing_area();
+//     root_area.fill(&WHITE).unwrap();
+
+//     let mut ctx = ChartBuilder::on(&root_area)
+//         .set_label_area_size(LabelAreaPosition::Left, 40)
+//         .set_label_area_size(LabelAreaPosition::Bottom, 40)
+//         .caption("Elevation Data", ("sans-serif", 40))
+//         .build_cartesian_2d::<Range<usize>, Range<f32>>(
+//             0..sample_count,
+//             min - lower_padding..max + upper_padding,
+//         )
+//         .unwrap();
+
+//     ctx.configure_mesh().draw().unwrap();
+
+//     ctx.draw_series(LineSeries::new(
+//         smoothed_data.iter().enumerate().map(|(x, y)| (x, *y)),
+//         &RED,
+//     ));
+//     ctx.draw_series(LineSeries::new(
+//         original_data.iter().enumerate().map(|(x, y)| (x, *y)),
+//         &BLUE,
+//     ))
+//     .unwrap();
+// }
